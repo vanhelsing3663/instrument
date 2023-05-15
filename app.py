3669d66638393952e6a8540d378dde621a20f6e9
Kirill <cyrillekonnov@yandex.ru>
2023-05-15 09:33:15 +0300

    .

--- app.py
+++ app.py
@@ -18,7 +18,10 @@ validator_email = CheckUserEmail()
 db = SQLAlchemy(app)
 
 
-
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(256), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
 
 
 @app.route('/')
