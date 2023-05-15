106b9f39349ce555ca0773e82b9b3a35007c0f0f
Kirill <cyrillekonnov@yandex.ru>
2023-05-15 10:10:23 +0300

    откат

--- app.py
+++ app.py
@@ -22,14 +22,6 @@ class Users(db.Model):
     password = db.Column(db.String(256), nullable=False)
 
 
-
-
-
-
-
-
-
-
 @app.route('/register', methods=["GET", "POST"])
 def registration_handler():
     """Примерная страница регистрации в будущем будет меняться"""
